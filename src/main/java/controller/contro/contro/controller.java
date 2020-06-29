981c6b5474a9278128738f3488050e86c8365207
pengjingbang <752679464@qq.com>
2020-06-29 19:41:56 +0800

    添加本地仓库

--- src/main/java/controller/contro/contro/controller.java
+++ src/main/java/controller/contro/contro/controller.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class controller {
 @RequestMapping("/hrhr")//访问http://localhost:6969/hrhr
     public String main(){
-        System.out.println("控制器000000000000");
+        System.out.println("控制器01111111111110");
         return "studyhtml" ;
     }
 }
